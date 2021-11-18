ae61c177997d59c529f2b56ec93d2c22c5c2a8ca
Buğra Akca <bgrakca@gmail.com>
2021-11-18 17:34:27 +0300

    Inverting the brightness values and some readibility changes

--- src/ascii_art.py
+++ src/ascii_art.py
@@ -1,16 +1,17 @@
 from PIL import Image
 from colorama import Fore
 
-
+# Get the file path
 file_path = "images/ww.jpg"
 
+# Define the ASCII characters and Max Pixel Value
 ASCII_CHARS = ' .,:°*oO&$'
 MAX_PIXEL_VALUE = 256
 
 
 # Open an image as image object
 image = Image.open(file_path)
-image.thumbnail((600, 200))
+image.thumbnail((600, 200)) # Resize the image to a smaller size
 
 # Print the image information
 print(image.format, image.size, image.mode)
@@ -28,31 +29,39 @@ def get_pixel_data():
         all_pixels.append(pixels[x:x+image.width])
     return all_pixels
 
-#* Convert RGB pixel data to Brightness value
-def rgb_to_brightness(algo = "avarage"):
+#* Convert RGB pixel data to Brightness value with different algorithms
+def rgb_to_brightness(algo = "avarage", ):
     '''Converting RGB pixel data to brightness values'''
     list_of_brightness = list()
-    temporay_list = list()
+    temporary_list = list()
     for in_list in get_pixel_data():
         for tuple in in_list:
-            if algo == "avarage":
-                tuple = (tuple[0]+tuple[1]+tuple[2]) // 3
-                temporay_list.append(tuple)
-            if algo == "luminosity":
-                tuple = (0.21 * tuple[0]) + (0.72 * tuple[1]) + (0.07 * tuple[2])
-                temporay_list.append(tuple)
-            if algo == "lightness":
-                tuple = (max(tuple[0], tuple[1], tuple[2]) + min(tuple[0], tuple[1], tuple[2])) / 2
-                temporay_list.append(tuple)
-    for i in range(0, len(temporay_list), image.width):
-        list_of_brightness.append(temporay_list[i:i+image.width])
+            if algo == "avarage": # Avarage Algorithm
+                tuple = round((tuple[0]+tuple[1]+tuple[2]) // 3)
+            elif algo == "luminosity": # luminosity Algorithm
+                tuple = round((0.21 * tuple[0]) + (0.72 * tuple[1]) + (0.07 * tuple[2]))
+            elif algo == "lightness": # Lightness Algorithm
+                tuple = round((max(tuple[0], tuple[1], tuple[2]) + min(tuple[0], tuple[1], tuple[2])) / 2)
+            else: # Raise an error if the algo is not recognized
+                raise Exception(f"Unrecognized algo_name: {algo}")
+            temporary_list.append(tuple)
+    # Adding the tuples to the list, creating a 2d array.
+    for i in range(0, len(temporary_list), image.width):
+        list_of_brightness.append(temporary_list[i:i+image.width])
     return list_of_brightness
+
+def invert_brightness(brightness_values = rgb_to_brightness()):
+    '''Inverting the brightness values'''
+    inverted_brightness_values = list()
+    for inverted in brightness_values:
+        inverted_brightness_values.append([MAX_PIXEL_VALUE - x for x in inverted])
+    return inverted_brightness_values
 
 def create_ascii_art(color = Fore.WHITE):
     '''Turning brightness values into an ASCII strings. An print it out.'''
-    for br in rgb_to_brightness():
+    for br in invert_brightness():
         for x in br:
-            x = ((x * len(ASCII_CHARS) // MAX_PIXEL_VALUE))
+            x = ((x * len(ASCII_CHARS) // MAX_PIXEL_VALUE)) # I divide 256 because the pixel values is 0-255 with zero included. That is give me a 256.
             ascii_val = ASCII_CHARS[x]
             print(color + ascii_val, end='  ') # I added two spaces between the characters so that the image doesn't look squashed.
         # Printing new line is necessary for this method. Otherwise, the image
@@ -83,7 +92,8 @@ def create_ascii_art(color = Fore.WHITE):
 #     im.show()
 
 get_pixel_data()
-rgb_to_brightness(algo = "brightness")
+# rgb_to_brightness(algo = "luminosity")
+invert_brightness(rgb_to_brightness(algo = "luminosity"))
 create_ascii_art()
 # get_pixel_data_2()
 # put_pixels()
